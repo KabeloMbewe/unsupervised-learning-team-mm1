b194f241392f3c766618e48c1c5d1c04d63336a0
Hercules <hercosmith03@gmail.com>
2021-07-13 17:15:12 +0200

    Setup main as grid search

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 from surprise import SVD, SVDpp
 from surprise import Dataset, Reader
 from surprise import accuracy
-from surprise.model_selection import cross_validate, train_test_split
+from surprise.model_selection import cross_validate, train_test_split, GridSearchCV
 
-from surprise.dump import load
-import pandas as pd
+reader = Reader(
+    name=None,
+    line_format="user item rating",
+    sep=",",
+    rating_scale=(1, 5),
+    skip_lines=1,
+)
 
-print("Loading model...")
-algo: SVD = load("model.pkl")
+print("Loading data...")
+# data = Dataset.load_from_file("data/train.csv", reader)
+data = Dataset.load_builtin(name="ml-100k")
 
-print("Loading prediction data...")
-test = pd.read_csv("data/test.csv")
+param_grid = {
+    "n_factors": [10, 50, 100, 200, 300],
+    "n_epochs": [20],
+    "biased": [True, False],
+    "init_mean": [0, 3.53],
+    "init_std_dev": [0.1, 1.06],
+    "lr_all": [0.004, 0.005, 0.006],
+    "reg_all": [0.01, 0.02, 0.03],
+    "lr_bu": [None],
+    "lr_bi": [None],
+    "lr_pu": [None],
+    "lr_qi": [None],
+    "reg_bu": [None],
+    "reg_bi": [None],
+    "reg_pu": [None],
+    "reg_qi": [None],
+}
 
-preds = dict()
+gs = GridSearchCV(
+    SVD, param_grid, measures=["rmse", "mae"], cv=5, n_jobs=-1, joblib_verbose=2
+)
 
-print("Predicting...")
-total = len(test)
+print("Performing grid search...")
+gs.fit(data)
 
-for idx, (uid, mid) in enumerate(test.to_records(index=False)):
-    prediction = algo.predict(uid, mid).est
+# best RMSE score
+print(gs.best_score["rmse"])
 
-    pred_id = f"{uid}_{mid}"
-    preds[pred_id] = prediction
-
-    if ((idx + 1) % 10_000) == 0:
-        print("[progress]", round(idx / total * 100.0, 1), "%")
-
-pd.Series(preds).to_csv("submission.csv")
+# combination of parameters that gave the best RMSE score
+print(gs.best_params["rmse"])
