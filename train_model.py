460e116f31be46324070db8c724b3f6a986d03d8
Hercules <hercosmith03@gmail.com>
2021-07-13 13:04:22 +0200

    Basic Model training script that also dumps the model to 'model.pkl'

---
+++ train_model.py
@@ -0,0 +1,26 @@
+from surprise import SVD, SVDpp
+from surprise import Dataset, Reader
+from surprise.model_selection import cross_validate, train_test_split
+
+from surprise.dump import dump
+
+reader = Reader(
+    name=None,
+    line_format="user item rating",
+    sep=",",
+    rating_scale=(1, 5),
+    skip_lines=1,
+)
+
+print("Loading data...")
+data = Dataset.load_from_file("data/train.csv", reader)
+
+print("Building training file")
+train_set = data.build_full_trainset()
+algo = SVD()
+
+print("Fitting...")
+algo.fit(train_set)
+
+print("Dumping...")
+dump("model.pkl", algo=algo, verbose=1)
