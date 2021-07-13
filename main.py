da41cffb637e59ce34b14a90f84503edbfd81abb
Hercules <hercosmith03@gmail.com>
2021-07-13 08:35:49 +0200

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from surprise import SVD, SVDpp
 from surprise import Dataset, Reader
-from surprise.model_selection import cross_validate
+from surprise import accuracy
+from surprise.model_selection import cross_validate, train_test_split
 
 reader = Reader(
     name=None,
@@ -13,16 +14,26 @@ reader = Reader(
 print("Loading data...")
 data = Dataset.load_from_file("data/train.csv", reader)
 
-algo = SVD()
+print("Building sets...")
+train_set, test_set = train_test_split(data, test_size=0.25)
 
-print("Running cross-validation")
-cross_validate(
-    algo=algo,
-    data=data,
-    measures=["RMSE", "MAE"],
-    cv=5,
-    return_train_measures=True,
-    verbose=True,
-    n_jobs=1,
-    # pre_dispatch=4,
-)
+algo = SVDpp(n_factors=1)
+
+print("Fitting...")
+algo.fit(train_set)
+
+print("Testing...")
+predictions = algo.test(testset)
+print("RMSE:", accuracy.rmse(predictions))
+
+# print("Running cross-validation")
+# cross_validate(
+#     algo=algo,
+#     data=data,
+#     measures=["RMSE", "MAE"],
+#     cv=1,
+#     return_train_measures=True,
+#     verbose=True,
+#     n_jobs=1,
+#     pre_dispatch=2,
+# )
