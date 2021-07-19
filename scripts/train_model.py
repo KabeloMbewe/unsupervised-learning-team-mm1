81ec85eb42d9c90f52bf593eee5867503b249391
Hercules <hercosmith03@gmail.com>
2021-07-19 16:42:46 +0200

    Cleanup Scripts

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -25,19 +25,13 @@ del data
 del reader
 
 algo = SVD(
-    n_factors=10,
-    n_epochs=46,
-    biased=True,
-    init_mean=3.0352,
-    init_std_dev=0.3539,
-    lr_bu=0.008,
-    lr_bi=0.0204,
-    lr_pu=0.0176,
-    lr_qi=0.0173,
-    reg_bu=0.0896,
-    reg_bi=0.0505,
-    reg_pu=0.077,
-    reg_qi=0.0928,
+    n_factors=200,
+    n_epochs=30,
+    lr_all=0.005,
+    reg_all=0.02,
+    init_std_dev=0.02,
+    random_state=42,
+    verbose=True,
 )
 
 print("Fitting...")
